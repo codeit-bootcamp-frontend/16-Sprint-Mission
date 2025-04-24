180b6c0251eaf81055fa696e38339ee42d6ea46b
leesangdal <asksa1256@gmail.com>
2025-04-24 18:00:29 +0900

    feat: 로그인, 회원가입 자동완성 인식 제거

--- script/login.js
+++ script/login.js
@@ -31,14 +31,9 @@ form.addEventListener("focusout", formValidate);
 /* 비밀번호 토글 */
 togglePasswordHandler(form);
 
-/* 페이지 진입 UX */
+/* UX: 첫번째 input focus 처리 */
 window.addEventListener("DOMContentLoaded", () => {
-  // 첫번째 input focus 처리
   form.querySelector(".form-input").focus();
-
-  // 페이지 진입 시, 폼 자동완성 되어있으면 버튼 활성화
-  loginBtn.disabled =
-    emailInput.value.length === 0 && passwordInput.value.length === 0;
 });
 
 /* 로그인 버튼 클릭 시 'items'로 이동 */
