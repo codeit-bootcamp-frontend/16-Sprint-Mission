f8ff15621bbd42a6671a2f537ba89a444aab461c
Chiman2937 <cyga2933@gmail.com>
2025-04-24 19:39:48 +0900

    perf(js): 비밀번호 토글 아이콘 EventHandler모듈로 통합

--- scripts/auth/form_login.js
+++ scripts/auth/form_login.js
@@ -1,32 +1,27 @@
-import { InputEventHandler, inputValidState } from './modules/InputEventHandler.js';
+import { InputEventHandler, inputValidState, onPasswordIconClick } from './modules/EventHandler.js';
 
-const imgPasswordVisible = document.querySelector('.form-icon-password');
 const formButton = document.querySelector('.form-btn');
 const useValidState = ['email', 'password'];
 
-// 비밀번호 눈 아이콘 클릭 이벤트
-function onPasswordIconClick(e) {
-  imgPasswordVisible.classList.toggle('inVisible');
-  const inputPassword = document.querySelector(`#password .form-input`);
-  if (imgPasswordVisible.contains('inVisible')) inputPassword.type = 'password';
-  else inputPassword.type = 'text';
-}
-
 // 로그인 버튼 클릭 이벤트
-function onLoginButtonClick(e) {
+const onLoginButtonClick = (e) => {
   e.preventDefault();
   location.href = 'items.html';
 }
 
-// input focusout 이벤트 일괄 등록
+// 이벤트 일괄 등록
 for (const stateKey of useValidState) {
   // inputValidState 초기화
   inputValidState[stateKey].isValid = false;
   const inputElement = document.querySelector(`#${stateKey} .form-input`);
+
   // InputEventHandler 모듈 함수 할당
   inputElement.addEventListener('input', () => InputEventHandler(stateKey));
   inputElement.addEventListener('focusout', () => InputEventHandler(stateKey));
+  
+  // 눈 모양 아이콘 모듈 함수 할당
+  const togglePasswordIcon = document.querySelector(`#${stateKey} .form-icon-password`);
+  if(togglePasswordIcon) togglePasswordIcon.addEventListener('click', (e) => onPasswordIconClick(e, stateKey));
 }
 
-imgPasswordVisible.addEventListener('click', onPasswordIconClick);
 formButton.addEventListener('click', onLoginButtonClick);
--- scripts/auth/form_signup.js
+++ scripts/auth/form_signup.js
@@ -1,45 +1,29 @@
-import { InputEventHandler, inputValidState } from './modules/InputEventHandler.js';
-
-const imgPasswordVisible = document.querySelector('#password .form-icon-password');
-const imgPasswordVerifyVisible = document.querySelector('#passwordVerify .form-icon-password');
+import { InputEventHandler, inputValidState, onPasswordIconClick } from './modules/EventHandler.js';
 
 const formButton = document.querySelector('.form-btn');
 
 //inputValidState에서 사용할 Key 선택
 const useValidState = ['email', 'nickname', 'password', 'passwordVerify'];
 
-// 비밀번호 눈 아이콘 클릭 이벤트
-function onPasswordIconClick() {
-  imgPasswordVisible.classList.toggle('inVisible');
-  const inputPassword = document.querySelector(`#password .form-input`);
-  if (imgPasswordVisible.classList.contains('inVisible')) inputPassword.type = 'password';
-  else inputPassword.type = 'text';
-}
-
-// 비밀번호 확인 눈 아이콘 클릭 이벤트
-function onPasswordVerifyIconClick() {
-  imgPasswordVerifyVisible.classList.toggle('inVisible');
-  const inputPasswordVerify = document.querySelector(`#passwordVerify .form-input`);
-  if (imgPasswordVerifyVisible.classList.contains('inVisible')) inputPasswordVerify.type = 'password';
-  else inputPasswordVerify.type = 'text';
-}
-
 // 회원가입 버튼 클릭 이벤트
-function onSignupButtonClick(e) {
+const onSignupButtonClick = (e) => {
   e.preventDefault();
   location.href = 'login.html';
 }
 
-// input focusout 이벤트 일괄 등록
+// 이벤트 일괄 등록
 for (const stateKey of useValidState) {
   // inputValidState 초기화
   inputValidState[stateKey].isValid = false;
   const inputElement = document.querySelector(`#${stateKey} .form-input`);
+
   // InputEventHandler 모듈 함수 할당
   inputElement.addEventListener('input', () => InputEventHandler(stateKey));
   inputElement.addEventListener('focusout', () => InputEventHandler(stateKey));
+  
+  // 눈 모양 아이콘 모듈 함수 할당
+  const togglePasswordIcon = document.querySelector(`#${stateKey} .form-icon-password`);
+  if(togglePasswordIcon) togglePasswordIcon.addEventListener('click', (e) => onPasswordIconClick(e, stateKey));
 }
 
-imgPasswordVisible.addEventListener('click', onPasswordIconClick);
-imgPasswordVerifyVisible.addEventListener('click', onPasswordVerifyIconClick);
 formButton.addEventListener('click', onSignupButtonClick);
