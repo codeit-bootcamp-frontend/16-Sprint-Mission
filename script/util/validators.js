9c2c931e988d04b142d9365be7020e96cacffb4c
leesangdal <asksa1256@gmail.com>
2025-04-24 16:24:56 +0900

    refactor: 유효성 상태 설정 함수 합치기

--- script/util/validators.js
+++ script/util/validators.js
@@ -1,29 +1,30 @@
 "use strict";
 
-function setInvalid(input, message) {
+/* 유효성 상태 설정 */
+function setValid(input, valid = true, msg) {
   const inputHintEl = input
     .closest(".form-control")
     .querySelector(".form-input-hint");
-  input.classList.add("invalid");
-  inputHintEl.textContent = message;
-}
 
-function setValid(input) {
-  const inputHintEl = input
-    .closest(".form-control")
-    .querySelector(".form-input-hint");
+  if (!valid) {
+    input.classList.add("invalid");
+    inputHintEl.textContent = msg;
+    return;
+  }
+
   input.classList.remove("invalid");
   inputHintEl.textContent = "";
 }
 
+/* 이메일 유효성 */
 function validateEmail(input) {
   if (input.value.trim().length === 0) {
-    setInvalid(input, "이메일을 입력해주세요.");
+    setValid(input, false, "이메일을 입력해주세요.");
     return false;
   }
 
   if (!input.checkValidity()) {
-    setInvalid(input, "잘못된 이메일 형식입니다.");
+    setValid(input, false, "잘못된 이메일 형식입니다.");
     return false;
   }
 
@@ -31,6 +32,7 @@ function validateEmail(input) {
   return true;
 }
 
+/* 비밀번호 유효성 */
 function validatePassword(input, chkInput) {
   // 비밀번호 체크 연동
   if (chkInput) {
@@ -43,12 +45,12 @@ function validatePassword(input, chkInput) {
 
   // 비밀번호 유효성 검사
   if (input.value.trim().length === 0) {
-    setInvalid(input, "비밀번호를 입력해주세요.");
+    setValid(input, false, "비밀번호를 입력해주세요.");
     return false;
   }
 
   if (input.value.trim().length < 8) {
-    setInvalid(input, "비밀번호를 8자 이상 입력해주세요.");
+    setValid(input, false, "비밀번호를 8자 이상 입력해주세요.");
     return false;
   }
 
@@ -56,9 +58,10 @@ function validatePassword(input, chkInput) {
   return true;
 }
 
+/* 비밀번호 확인 유효성 */
 function validatePasswordChk(input, chkInput) {
   if (input.value !== chkInput.value) {
-    setInvalid(chkInput, "비밀번호가 일치하지 않습니다.");
+    setValid(chkInput, false, "비밀번호가 일치하지 않습니다.");
     return false;
   }
 
@@ -66,9 +69,10 @@ function validatePasswordChk(input, chkInput) {
   return true;
 }
 
+/* 닉네임 유효성 */
 function validateNickname(input) {
   if (input.value.trim().length === 0) {
-    setInvalid(input, "닉네임을 입력해주세요.");
+    setValid(input, false, "닉네임을 입력해주세요.");
     return false;
   }
 
