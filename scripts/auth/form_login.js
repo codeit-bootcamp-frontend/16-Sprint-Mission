3a429f742513626adc5e6650b4a0a5bc3d20c3c4
Chiman2937 <cyga2933@gmail.com>
2025-04-24 20:19:15 +0900

    perf(js): 불필요한 useValidState 변수 삭제

--- scripts/auth/form_login.js
+++ scripts/auth/form_login.js
@@ -1,7 +1,6 @@
 import { InputEventHandler, inputValidState, onPasswordIconClick } from './modules/EventHandler.js';
 
 const formButton = document.querySelector('.form-btn');
-const useValidState = ['email', 'password'];
 
 // 로그인 버튼 클릭 이벤트
 const onLoginButtonClick = (e) => {
@@ -10,14 +9,16 @@ const onLoginButtonClick = (e) => {
 }
 
 // 이벤트 일괄 등록
-for (const stateKey of useValidState) {
-  // inputValidState 초기화
-  inputValidState[stateKey].isValid = false;
+for (const stateKey in inputValidState) {
   const inputElement = document.querySelector(`#${stateKey} .form-input`);
-
-  // InputEventHandler 모듈 함수 할당
-  inputElement.addEventListener('input', () => InputEventHandler(stateKey));
-  inputElement.addEventListener('focusout', () => InputEventHandler(stateKey));
+  if(inputElement) {
+    // inputValidState 초기화
+    inputValidState[stateKey].isValid = false;
+    
+    // InputEventHandler 모듈 함수 할당
+    inputElement.addEventListener('input', () => InputEventHandler(stateKey));
+    inputElement.addEventListener('focusout', () => InputEventHandler(stateKey));
+  }
   
   // 눈 모양 아이콘 모듈 함수 할당
   const togglePasswordIcon = document.querySelector(`#${stateKey} .form-icon-password`);
--- scripts/auth/form_signup.js
+++ scripts/auth/form_signup.js
@@ -2,9 +2,6 @@ import { InputEventHandler, inputValidState, onPasswordIconClick } from './modul
 
 const formButton = document.querySelector('.form-btn');
 
-//inputValidState에서 사용할 Key 선택
-const useValidState = ['email', 'nickname', 'password', 'passwordVerify'];
-
 // 회원가입 버튼 클릭 이벤트
 const onSignupButtonClick = (e) => {
   e.preventDefault();
@@ -12,14 +9,16 @@ const onSignupButtonClick = (e) => {
 }
 
 // 이벤트 일괄 등록
-for (const stateKey of useValidState) {
-  // inputValidState 초기화
-  inputValidState[stateKey].isValid = false;
+for (const stateKey in inputValidState) {
   const inputElement = document.querySelector(`#${stateKey} .form-input`);
-
-  // InputEventHandler 모듈 함수 할당
-  inputElement.addEventListener('input', () => InputEventHandler(stateKey));
-  inputElement.addEventListener('focusout', () => InputEventHandler(stateKey));
+  if(inputElement) {
+    // inputValidState 초기화
+    inputValidState[stateKey].isValid = false;
+    
+    // InputEventHandler 모듈 함수 할당
+    inputElement.addEventListener('input', () => InputEventHandler(stateKey));
+    inputElement.addEventListener('focusout', () => InputEventHandler(stateKey));
+  }
   
   // 눈 모양 아이콘 모듈 함수 할당
   const togglePasswordIcon = document.querySelector(`#${stateKey} .form-icon-password`);
