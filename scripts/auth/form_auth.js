c90ab802827ecb9ba94db6d9c856417bc0ff8d28
Chiman2937 <cyga2933@gmail.com>
2025-04-26 16:47:57 +0900

    refactor(js): message 변수 객체화

--- scripts/auth/form_auth.js
+++ scripts/auth/form_auth.js
@@ -1,19 +1,6 @@
-import { getState, setState, inputEventHandler, onPasswordIconClick } from './modules/EventHandler.js';
+import { getState, setState, inputEventHandler, onPasswordIconClick, onButtonClick } from './modules/EventHandler.js';
 
 const formButton = document.querySelector('.form-btn');
-
-// 버튼 클릭 이벤트
-const onButtonClick = (e) => {
-  const formType = document.querySelector(".form-container").dataset.mode;
-  if(formType==="login"){
-    e.preventDefault();
-    location.href = 'items.html';
-  }else if(formType==="signup"){
-    e.preventDefault();
-    location.href = 'login.html';
-  }
-}
-
 const inputState = getState();
 
 // 이벤트 일괄 등록
@@ -21,8 +8,7 @@ for (const stateKey in inputState) {
   const inputElement = document.querySelector(`#${stateKey} .form-input`);
   if(inputElement) {
     // inputValidState 초기화
-    setState(stateKey, { isValid: false })
-    // inputValidState[stateKey].isValid = false;
+    setState(stateKey, { isValid: null })
 
     // inputEventHandler 모듈 함수 할당
     inputElement.addEventListener('input', () => inputEventHandler(stateKey));
