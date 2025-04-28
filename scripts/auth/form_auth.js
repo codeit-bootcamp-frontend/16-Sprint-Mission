3b2ecb5082c25cc1f984f68212eec1828c4bc768
Chiman2937 <cyga2933@gmail.com>
2025-04-28 20:08:23 +0900

    refactor(js): 코드리뷰 반영

--- scripts/auth/form_auth.js
+++ scripts/auth/form_auth.js
@@ -1,23 +1,85 @@
-import { getState, setState, inputEventHandler, onPasswordIconClick, onButtonClick } from './modules/EventHandler.js';
+import { getState, setState, setValidation } from './modules/EventHandler.js';
 
 const formButton = document.querySelector('.form-btn');
-const inputState = getState();
+
+const inputEventHandler = (stateKey) => {
+  const inputValidState = getState();
+  setValidation(stateKey);
+  updateUIByState(stateKey);
+  if (
+    stateKey === 'password' &&
+    inputValidState['passwordVerify'].isExist &&
+    inputValidState['passwordVerify'].isValid !== null
+  )
+    inputEventHandler('passwordVerify');
+  setButtonState();
+};
+
+const onPasswordIconClick = (e, stateKey) => {
+  e.target.classList.toggle('inVisible');
+  const inputElement = document.querySelector(`#${stateKey} .form-input`);
+  if (e.target.classList.contains('inVisible')) inputElement.type = 'password';
+  else inputElement.type = 'text';
+};
+
+/**
+ * 버튼 클릭 이벤트 지정 함수
+ */
+const onButtonClick = (e) => {
+  const formType = document.querySelector('.form-container').dataset.mode;
+  if (formType === 'login') {
+    e.preventDefault();
+    location.href = 'items.html';
+  } else if (formType === 'signup') {
+    e.preventDefault();
+    location.href = 'login.html';
+  }
+};
+
+const updateUIByState = (stateKey) => {
+  const inputValidState = getState();
+  //prettier-ignore
+  const inputContainerElement = document.querySelector(`#${stateKey} .form-input-container`);
+  //prettier-ignore
+  const spanStateElement = document.querySelector(`#${stateKey} .form-status-info`);
+  if (inputValidState[stateKey].isValid) {
+    inputContainerElement.classList.add('valid');
+    inputContainerElement.classList.remove('inValid');
+    spanStateElement.classList.add('inVisible');
+  } else {
+    inputContainerElement.classList.add('inValid');
+    inputContainerElement.classList.remove('valid');
+    spanStateElement.classList.remove('inVisible');
+  }
+  spanStateElement.textContent = inputValidState[stateKey].message;
+};
+
+const setButtonState = () => {
+  const inputValidState = getState();
+  const formButton = document.querySelector('.form-btn');
+  formButton.disabled = !Object.values(inputValidState).every((v) => v.isValid);
+};
 
 // 이벤트 일괄 등록
-for (const stateKey in inputState) {
+for (const stateKey in getState()) {
   const inputElement = document.querySelector(`#${stateKey} .form-input`);
-  if(inputElement) {
+  if (inputElement) {
     // inputValidState 초기화
-    setState(stateKey, { isValid: null })
+    setState(stateKey, { isExist: true, isValid: null });
 
     // inputEventHandler 모듈 함수 할당
+    //prettier-ignore
     inputElement.addEventListener('input', () => inputEventHandler(stateKey));
+    //prettier-ignore
     inputElement.addEventListener('focusout', () => inputEventHandler(stateKey));
   }
-  
+
   // 눈 모양 아이콘 모듈 함수 할당
-  const togglePasswordIcon = document.querySelector(`#${stateKey} .form-icon-password`);
-  if(togglePasswordIcon) togglePasswordIcon.addEventListener('click', (e) => onPasswordIconClick(e, stateKey));
+  //prettier-ignore
+  const passwordIcon = document.querySelector(`#${stateKey} .form-icon-password`);
+  if (passwordIcon)
+    //prettier-ignore
+    passwordIcon.addEventListener('click', (e) => onPasswordIconClick(e, stateKey));
 }
 
 formButton.addEventListener('click', onButtonClick);
--- scripts/auth/modules/EventHandler.js
+++ scripts/auth/modules/EventHandler.js
@@ -1,137 +1,81 @@
 // input 상태관리 변수
 const inputValidState = {
-  email: { isValid: true, message: '' },
-  nickname: { isValid: true, message: '' },
-  password: { isValid: true, message: '' },
-  passwordVerify: { isValid: true, message: '' },
+  email: { isValid: true, isExist: false, message: '' },
+  nickname: { isValid: true, isExist: false, message: '' },
+  password: { isValid: true, isExist: false, message: '' },
+  passwordVerify: { isValid: true, isExist: false, message: '' },
 };
-
-/**
- * input 요소에서 focusout 이벤트 발생 시 관련 요소의 정보를 입력받아 이벤트를 처리하는 함수
- * @param {string} statusKey inputStatus 중 이벤트가 발생한 input 요소의 key
- */
-export const inputEventHandler = (stateKey) => {
-  validateHandler(stateKey);
-  matchPasswordHandler(stateKey);
-  updateUIByState(stateKey);
-}
-
-/**
- * 유효성검사 결과값을 InputValidState에 업데이트하는 함수
- * @param {string} stateKey 
- */
-const validateHandler = (stateKey) => {
-  const inputElement = document.querySelector(`#${stateKey} .form-input`);
-  const isInputValid = inputValidators[stateKey](inputElement.value);
-  const validMessage = messageObject[stateKey][isInputValid];
-  setState(stateKey, {isValid: isInputValid, message: validMessage});
-}
-
-/**
- * passwordVerify가 유효성검사를 통과했을 때 password와 일치하는지 확인하고, passwordVerify의 스타일을 업데이트 하는 함수
- * password 또는 passwordVerify가 변경될 때만 실행
- * @param {string} stateKey
- * @returns 
- */
-const matchPasswordHandler = (stateKey) => {
-  if (!(stateKey=== 'password' || stateKey === 'passwordVerify')) return;
-  if (!document.querySelector('#passwordVerify .form-input')) return;
-  validateHandler('passwordVerify');
-  if(getState('passwordVerify').isValid){
-    const isMatched = inputMatchers.passwordMatch();
-    const matchedMessage = messageObject.passwordMatch[isMatched];
-    setState('passwordVerify',{isValid: isMatched, message: matchedMessage});
-    updateUIByState('passwordVerify');
-  }
-}
 
 const messageObject = {
   email: {
     null: '이메일을 입력해주세요',
     false: '잘못된 이메일 형식입니다',
-    true: ''
+    true: '',
   },
   nickname: {
     null: '닉네임을 입력해주세요',
     false: '잘못된 이메일 형식입니다',
-    true: ''
+    true: '',
   },
   password: {
     null: '비밀번호를 입력해주세요',
     false: '비밀번호를 8자 이상 입력해주세요',
-    true: ''
+    true: '',
   },
   passwordVerify: {
     null: '비밀번호를 입력해주세요',
     false: '비밀번호를 8자 이상 입력해주세요',
-    true: ''
+    true: '',
   },
   passwordMatch: {
     false: '비밀번호가 일치하지 않습니다',
-    true: ''
+    true: '',
+  },
+};
+
+/**
+ * 유효성검사 결과값을 InputValidState에 업데이트하는 함수
+ * @param {string} stateKey
+ */
+export const setValidation = (stateKey) => {
+  const inputElement = document.querySelector(`#${stateKey} .form-input`);
+  let isInputValid = inputValidators[stateKey](inputElement.value);
+  let validMessage = messageObject[stateKey][isInputValid];
+  if (stateKey === 'passwordVerify' && isInputValid) {
+    const passwordText = document.querySelector(`#password .form-input`).value;
+    //prettier-ignore
+    isInputValid = inputValidators['passwordMatch'](passwordText, inputElement.value);
+    validMessage = messageObject['passwordMatch'][isInputValid];
   }
-}
+  setState(stateKey, { isValid: isInputValid, message: validMessage });
+};
 
 /**
  * 유효성 검사 함수
  */
 const inputValidators = {
   email: (emailText) => {
-    const emailRegex = new RegExp(/^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i);
+    const emailRegex = new RegExp(
+      /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i
+    );
     if (!emailText) return null;
-    return (emailRegex.test(emailText));
+    return emailRegex.test(emailText);
   },
   nickname: (nicknameText) => {
     return !!nicknameText;
   },
   password: (passwordText) => {
     if (!passwordText) return null;
-    return (passwordText.length >= 8);
+    return passwordText.length >= 8;
   },
   passwordVerify: (passwordVerifyText) => {
     if (!passwordVerifyText) return null;
-    return (passwordVerifyText.length >= 8);
+    return passwordVerifyText.length >= 8;
+  },
+  passwordMatch: (passwordText, passwordVerifyText) => {
+    return passwordText === passwordVerifyText;
   },
 };
-
-/**
- * password, passwordVerify 일치 비교 함수수
- */
-const inputMatchers = {
-  passwordMatch: () => {
-    const passwordText = document.querySelector('#password .form-input').value;
-    const passwordVerifyText = document.querySelector('#passwordVerify .form-input').value;
-    return (passwordText === passwordVerifyText);
-  }
-}
-
-/**
- * inputValidState값에 따라 stateKey에 해당하는 요소의 스타일을 업데이트 해주는 함수
- * @param {string} stateKey 
- */
-const updateUIByState = (stateKey) => {
-  const inputContainerElement = document.querySelector(`#${stateKey} .form-input-container`);
-  const spanStateElement = document.querySelector(`#${stateKey} .form-status-info`);
-  if (inputValidState[stateKey].isValid) {
-    inputContainerElement.classList.add('valid');
-    inputContainerElement.classList.remove('inValid');
-    spanStateElement.classList.add('inVisible');
-  } else {
-    inputContainerElement.classList.add('inValid');
-    inputContainerElement.classList.remove('valid');
-    spanStateElement.classList.remove('inVisible');
-  }
-  spanStateElement.textContent = inputValidState[stateKey].message;
-  setButtonState();
-};
-
-/**
- * inputValidState값에 따라 버튼의 활성화/비활성화 여부를 결정하는 함수
- */
-const setButtonState = () => {
-  const formButton = document.querySelector('.form-btn');
-  formButton.disabled = !Object.values(inputValidState).every((v) => v.isValid);
-}
 
 /**
  * input 상태관리 변수 값을 불러오는 함수
@@ -139,38 +83,18 @@ const setButtonState = () => {
  * @returns 기본값: inputValidState 객체 return / stateKey 지정 시 : stateKey에 해당하는 상태객체 반환
  */
 export const getState = (stateKey) => {
-  if(stateKey) return inputValidState[stateKey];
+  if (stateKey) return inputValidState[stateKey];
   else return inputValidState;
-}
+};
 
 /**
- * input 상태관리 변수 값을 지정하는 함수수
+ * input 상태관리 변수 값을 지정하는 함수
  * @param {string} changedStateKey 변경 할 stateKey 지정
  * @param {object} changedStateObject 변경 할 stateKey의 값을 객체로 지정({isvalid: true/false, message: ''})
  */
 export const setState = (changedStateKey, changedStateObject) => {
-  for(const changeditem in changedStateObject){
-    inputValidState[changedStateKey][changeditem] = changedStateObject[changeditem];
+  for (const changeditem in changedStateObject) {
+    inputValidState[changedStateKey][changeditem] =
+      changedStateObject[changeditem];
   }
-}
-
-export const onPasswordIconClick = (e, stateKey) => {
-  e.target.classList.toggle('inVisible');
-  const inputElement = document.querySelector(`#${stateKey} .form-input`);
-  if (e.target.classList.contains('inVisible')) inputElement.type = 'password';
-  else inputElement.type = 'text';
-}
-
-/**
- * 버튼 클릭 이벤트 지정 함수
- */
-export const onButtonClick = (e) => {
-  const formType = document.querySelector(".form-container").dataset.mode;
-  if(formType==="login"){
-    e.preventDefault();
-    location.href = 'items.html';
-  }else if(formType==="signup"){
-    e.preventDefault();
-    location.href = 'login.html';
-  }
-}
+};
