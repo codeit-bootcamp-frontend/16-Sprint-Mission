3b367537ba97b7a93919e16858337117844b966b
김성주 <aaa8757k@gmail.com>
2025-04-30 14:27:20 +0900

    feature: 로그인 유효성 검사 구현
    index.html
    -시안대로 검사 실패 메세지 발생 시 인풋 간 거리 50px
    없을 때 24px 간격 유지, 토글 아이콘도 같이 이동할 수 있게 컨테이너 추가
    -<form> action속성 추가
    
    login.css
    -유효성 검사 실패 클래스 추가 (.error-message, .error-Line,
    .pass-button,.pass-button:hover)
    -기존 버튼 색 --secondary-gray400으로 변경.
    
    validation.js,toggle-visibility-pw.js
    -러프하게 기능 구현

---
+++ login/toggle-visibility-pw.js
@@ -0,0 +1,38 @@
+// #toggle-visibility-pw 관련 변수
+const visibilityPw = document.querySelector("#toggle-visibility-pw");
+const visibilityPwLabel = document.querySelector("label[for='toggle-visibility-pw']");
+const passwordInput = document.querySelector("#user-password");
+
+// #toggle-visibility-pwcheck 관련 변수
+const visibilityPwCheck = document.querySelector("#toggle-visibility-pwcheck");
+const visibilityPwCheckLabel = document.querySelector("label[for='toggle-visibility-pwcheck']");
+const pwCheckInput = document.querySelector("#user-password-check");
+
+
+function toggleVisibilityPw() {
+    let isShowing = visibilityPw.checked;
+    if (isShowing) {
+        passwordInput.type = "text";
+        visibilityPwLabel.setAttribute('aria-checked', 'true');
+    } else {
+        passwordInput.type = "password";
+        visibilityPwLabel.setAttribute('aria-checked', 'false');
+    }
+}
+
+function toggleVisibilityPwCheck() {
+    let isShowing = visibilityPwCheck.checked;
+    if (isShowing) {
+        pwCheckInput.type = "text";
+        visibilityPwCheckLabel.setAttribute('aria-checked', 'true');
+    } else {
+        pwCheckInput.type = "password";
+        visibilityPwCheckLabel.setAttribute('aria-checked', 'false');
+    }
+}
+
+visibilityPw.addEventListener("click", toggleVisibilityPw);
+if (visibilityPwCheck) visibilityPwCheck.addEventListener("click", toggleVisibilityPwCheck);
+
+
+
