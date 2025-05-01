0b5b0f1ee9376df7484a3e39fc559ea747bc2978
김성주 <>
2025-05-01 15:12:17 +0900

    상대경로로 바꾸기

--- login/login-main.js
+++ login/login-main.js
@@ -1,6 +1,6 @@
-import { createValidRule, ruleObj, emailInput, passwordInput } from '/modules/validation-rule.mjs';
-import { initValidation } from '/modules/validate.mjs';
-import { initPasswordVisibility, visibilityPw } from '/modules/toggle-visibility-pw.mjs';
+import { createValidRule, ruleObj, emailInput, passwordInput } from '../modules/validation-rule.mjs';
+import { initValidation } from '../modules/validate.mjs';
+import { initPasswordVisibility, visibilityPw } from '../modules/toggle-visibility-pw.mjs';
 
 const validRule = createValidRule({ emailInput, passwordInput }, ruleObj);
 initValidation(validRule);
--- sign_up/sign-up-main.js
+++ sign_up/sign-up-main.js
@@ -1,6 +1,6 @@
-import { createValidRule, ruleObj, emailInput, passwordInput, nameInput, passwordCheckInput } from '/modules/validation-rule.mjs';
-import { initValidation } from '/modules/validate.mjs';
-import { initPasswordVisibility, visibilityPw, visibilityPwCheck } from '/modules/toggle-visibility-pw.mjs';
+import { createValidRule, ruleObj, emailInput, passwordInput, nameInput, passwordCheckInput } from '../modules/validation-rule.mjs';
+import { initValidation } from '../modules/validate.mjs';
+import { initPasswordVisibility, visibilityPw, visibilityPwCheck } from '../modules/toggle-visibility-pw.mjs';
 
 const validRule = createValidRule({ emailInput, passwordInput, nameInput, passwordCheckInput }, ruleObj);
 initValidation(validRule);
