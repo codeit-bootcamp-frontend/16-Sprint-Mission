fc10cfc48e3829a9adb8d05609afc72a22accf59
leesangdal <asksa1256@gmail.com>
2025-05-16 14:35:06 +0900

    style: 상품 목록 반응형 - pageSize 갯수 조절

--- src/pages/ItemsPage.jsx
+++ src/pages/ItemsPage.jsx
@@ -1,13 +1,23 @@
 import ItemList from "../components/ItemList";
 import BestItemList from "../components/BestItemList";
+import { useEffect, useState } from "react";
+import useWindowDimensions from "../hooks/useWindowDimensions";
+
+const DESKTOP = 1200;
+const LAPTOP = 768;
 
 const ItemsPage = () => {
+  const { width } = useWindowDimensions();
+
   return (
     <section className="page-content">
-      <BestItemList title="베스트 상품" pageSize={4} />
+      <BestItemList
+        title="베스트 상품"
+        pageSize={width >= DESKTOP ? 4 : width >= LAPTOP ? 2 : 1}
+      />
       <ItemList
         title="전체 상품"
-        pageSize={10}
+        pageSize={width >= DESKTOP ? 10 : width >= LAPTOP ? 6 : 4}
         showSearch
         showItemAddBtn
         showOrderDropdown
