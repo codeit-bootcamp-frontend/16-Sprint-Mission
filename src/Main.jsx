990d700ed00dc8bdc1fb00e38e51c2845e4e5e3d
asksa1256@gmail.com <asksa1256@gmail.com>
2025-05-17 00:00:49 +0900

    fix: index 페이지 중고마켓으로 설정

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,6 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./components/App";
-import HomePage from "./pages/HomePage";
 import ItemsPage from "./pages/ItemsPage";
 import BoardPage from "./pages/BoardPage";
 
@@ -9,7 +8,7 @@ const Main = () => {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />}>
-          <Route index element={<HomePage />} />
+          <Route index element={<ItemsPage />} />
           <Route path="/items" element={<ItemsPage />} />
           <Route path="/board" element={<BoardPage />} />
         </Route>
--- src/pages/HomePage.jsx
+++
@@ -1,5 +0,0 @@
-const HomePage = () => {
-  return <section className="page-content">홈페이지</section>;
-};
-
-export default HomePage;
