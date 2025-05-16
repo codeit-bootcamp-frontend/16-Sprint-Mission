d40d23d65ae439523eb4c362648cb18bf46fb056
asksa1256@gmail.com <asksa1256@gmail.com>
2025-05-16 22:56:46 +0900

    fix: Pagination prev, next 기능 문제 해결

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -6,36 +6,59 @@ const Pagination = ({
   totalPage,
   onCurrentPage,
 }) => {
+  const [hasPrev, setHasPrev] = useState(false);
   const [hasNext, setHasNext] = useState(true);
   const [currentPages, setCurrentPages] = useState(
     Array.from({ length: paginationSize }, (_, i) => i + 1)
   );
 
-  const handlePaginationClick = (e) => {
+  const handlePageNumClick = (e) => {
     const currentPage = e.target.textContent;
     onCurrentPage(currentPage);
   };
 
-  const handlePrevPaginationClick = () => {};
+  const handlePrevPaginationClick = () => {
+    const currentFirstPage = currentPages[0];
+    const prevFirstPage = currentFirstPage - paginationSize;
+    const prevLastPage = prevFirstPage + paginationSize - 1;
+
+    let prevPages = Array.from(
+      { length: paginationSize },
+      (_, i) => prevFirstPage + i
+    );
+
+    setHasPrev(true);
+    setCurrentPages(prevPages);
+    loadFunc({ page: prevLastPage });
+
+    if (prevFirstPage === 1) setHasPrev(false);
+
+    setHasNext(true);
+  };
 
   const handleNextPaginationClick = () => {
     const paginationLength = currentPages.length;
     const currentLastPage = currentPages[paginationLength - 1];
     const nextFirstPage = currentLastPage + 1;
-    if (currentLastPage >= totalPage) {
-      setHasNext(false);
-      return;
-    }
-    setHasNext(true);
-    setCurrentPages((prev) => prev.map((page) => page + 5));
+    let nextPages = Array.from(
+      { length: paginationSize },
+      (_, i) => nextFirstPage + i
+    ).filter((page) => page <= totalPage); // totalPage 초과 방지
+
+    setHasPrev(true);
+    setCurrentPages(nextPages);
     loadFunc({ page: nextFirstPage });
+
+    if (nextPages.includes(totalPage)) setHasNext(false);
   };
 
   return (
     <div className="pagination">
-      <button onClick={handlePrevPaginationClick}>prev</button>
+      <button onClick={handlePrevPaginationClick} disabled={!hasPrev}>
+        prev
+      </button>
       {currentPages.map((page) => (
-        <button key={page} onClick={handlePaginationClick}>
+        <button key={page} onClick={handlePageNumClick}>
           {page}
         </button>
       ))}
