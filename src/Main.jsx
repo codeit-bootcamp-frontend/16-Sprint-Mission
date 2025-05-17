a539e29fd0958a6f3c5179c5890932429cb776c0
leesangdal <asksa1256@gmail.com>
2025-05-17 11:36:04 +0900

    상품 등록 페이지 라우터 추가

--- src/Main.jsx
+++ src/Main.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./components/App";
 import ItemsPage from "./pages/ItemsPage";
 import BoardPage from "./pages/BoardPage";
+import AddItemPage from "./pages/AddItemPage";
 
 const Main = () => {
   return (
@@ -11,6 +12,7 @@ const Main = () => {
           <Route index element={<ItemsPage />} />
           <Route path="/items" element={<ItemsPage />} />
           <Route path="/board" element={<BoardPage />} />
+          <Route path="/addItem" element={<AddItemPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
