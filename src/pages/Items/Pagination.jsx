bb0a8a46610cdaf420dd47b5618d0072f915b831
김성주 <aaa8757k@gmail.com>
2025-05-24 02:13:15 +0900

    페이지네이션 불필요한 useEffect제거

--- src/pages/Items/Pagination.jsx
+++ src/pages/Items/Pagination.jsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useState } from "react";
+import { useCallback, useContext, useEffect, useState } from "react";
 import { ProductAllContext } from "../../context/ProductAllContext";
 import styles from "../../styles/Pagination.module.css";
 
@@ -7,40 +7,49 @@ const PAGINATION_MAX = 5;
 function Pagination() {
   const { queryStrings, setQueryStrings, total } = useContext(ProductAllContext);
   const { page, pageSize } = queryStrings;
-  const [pageStart, setPageStart] = useState(1);
   const [pageList, setPageList] = useState([1, 2, 3, 4, 5]);
   const maxPageLength = Math.ceil(total / pageSize);
 
+  const getPaginationStart = useCallback((page)=>{
+      return Math.floor((page - 1) / PAGINATION_MAX) * PAGINATION_MAX + 1;
+  },[page])
+
+  const pageStart = getPaginationStart(page);
+
   //이전으로 가기(pageStart변경)
   function handlePrev() {
-    if (pageStart - PAGINATION_MAX >= 1) {
-      setPageStart((prev) => prev - PAGINATION_MAX);
+    const prevPageStart = pageStart - PAGINATION_MAX;
+    
+    if (prevPageStart >= 1) {
+      updatePagination(prevPageStart);
     }
   }
 
   //다음으로 가기(pageStart변경)
   function handleNext() {
-    if (maxPageLength >= pageStart + PAGINATION_MAX) {
-      setPageStart((prev) => prev + PAGINATION_MAX);
+    const nextPageStart = pageStart + PAGINATION_MAX;
+
+    if (maxPageLength >= nextPageStart) {
+      updatePagination(nextPageStart);
     }
   }
 
-  //pageStart 바뀌면 페이지네이션 바꾸기
-  useEffect(() => {
-    const visiblePages = new Array(PAGINATION_MAX);
+  //이전, 다음 누르면 페이지네이션 바꾸기
+  function updatePagination(newPageStart) {
+    const visiblePages = [];
 
     for (let i = 0; i < PAGINATION_MAX; i++) {
-      if (pageStart + i > maxPageLength) break;
-      visiblePages[i] = pageStart + i;
+      if (newPageStart + i > maxPageLength) break;
+      visiblePages.push(newPageStart + i);
     }
 
     setPageList(visiblePages);
 
     setQueryStrings((prev) => ({
       ...prev,
-      page: pageStart,
+      page: newPageStart,
     }));
-  }, [pageStart, maxPageLength]);
+  }
 
   // 페이지네이션 누르면 쿼리 상태 업데이트
   function handlePageRequest(e) {
@@ -58,7 +67,7 @@ function Pagination() {
       className={styles[`items__all-pagination`]}
     >
       <li
-        aria-label="이전 페이지"
+        aria-label="이전 페이지로 가기"
         className={styles.pagination__prev}
         onClick={handlePrev}
       >
@@ -69,13 +78,13 @@ function Pagination() {
           aria-label={`${i}페이지로 이동`}
           className={page == i ? styles[`pagination--activate`] : ""}
           onClick={handlePageRequest}
-          key={`${i}`}
+          key={`page${i}`}
         >
           {i}
         </li>
       ))}
       <li
-        aria-label="다음 페이지"
+        aria-label="다음 페이지로 가기"
         className={styles.pagination__next}
         onClick={handleNext}
       >
