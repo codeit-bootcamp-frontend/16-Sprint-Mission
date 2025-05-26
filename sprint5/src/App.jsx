9536701a17b134592401c6205cac815858241035
kimin0401 <inptp1996@gmail.com>
2025-05-26 12:29:21 +0900

    Add: Pagination

--- sprint5/src/App.jsx
+++ sprint5/src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import Navi from "./Navi.jsx";
 import ItemCard from "./ItemCard.jsx";
+import Pagination from "./Pagination.jsx";
 
 function App() {
   return (
@@ -13,6 +14,14 @@ function App() {
         <ItemCard />
         <ItemCard />
       </section>
+      <section>
+        <h2>전체 상품</h2>
+        <ItemCard />
+        <ItemCard />
+        <ItemCard />
+        <ItemCard />
+      </section>
+      <Pagination />
     </>
   );
 }
