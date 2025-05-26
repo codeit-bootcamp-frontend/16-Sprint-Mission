0f2333e67dc562eda7ab0fbf5e860aae54e54d92
kimin0401 <inptp1996@gmail.com>
2025-05-26 13:02:01 +0900

    Add: SearchBar

---
+++ sprint5/src/SearchBar.jsx
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+
+const SearchBar = ({ onSearch }) => {
+  const [keyword, setKeyword] = useState("");
+
+  const handleSearch = () => {
+    onSearch(keyword);
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="검핵할 상품을 입력해주세요"
+        value={keyword}
+        onChange={(e) => setKeyword(e.target.value)}
+      />
+    </div>
+  );
+};
+
+export default SearchBar;
