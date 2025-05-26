7d84d6ff547d3d18ee2778dee1ad2163bd5be37c
Hwigeon <bb7788@naver.com>
2025-05-26 21:25:44 +0900

    feat(useApi): api를 받아올때 로딩여부와 오류를 관리하는 커스텀 훅 제작

---
+++ src/hooks/useApi.tsx
@@ -0,0 +1,45 @@
+import { use, useEffect, useRef, useState } from 'react';
+// 컴포넌트에서 api를 호출할때 로딩, 실패, 결과를 반환함
+// data : api 호출 결과
+// loading : api 호출 중인지 여부
+// error : api 호출 실패 여부 및 에러 메시지
+
+// @param {Function} apiFunction - API 호출 함수
+// @param [] deps - 의존성 배열 (옵션)
+export default function useApi(apiFunction: () => Promise<any>, deps: any[]) {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+  const [data, setData] = useState<any>(null);
+  // useRef를 사용하여 컴포넌트가 마운트된 상태인지 확인
+  const isMouted = useRef(true);
+  useEffect(() => {
+    return () => {
+      // 컴포넌트가 언마운트될 때 isMouted를 false로 설정
+      isMouted.current = false;
+    };
+  }, []);
+
+  // apiFunction을 호출하여 데이터를 가져옴
+  useEffect(() => {
+    setLoading(true);
+    setError(null);
+    apiFunction()
+      .then((result) => {
+        if (isMouted.current) {
+          setData(result);
+        }
+      })
+      .catch((err) => {
+        if (isMouted.current) {
+          setError(err.message);
+        }
+      })
+      .finally(() => {
+        if (isMouted.current) {
+          setLoading(false);
+        }
+      });
+  }, deps);
+
+  return { data, loading, error };
+}
--- src/pages/MarketPage/BestProduct.tsx
+++ src/pages/MarketPage/BestProduct.tsx
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import style from './BestProducts.module.scss';
+import getMediaCount from '@/utils/getMediaCount';
 
 // 베스트 상품 컴포넌트
 function BestProducts() {
+  // 미디어 쿼리에서 베스트 상품의 개수를 가져옵니다.
+  const { bestProductsCount } = getMediaCount();
+
+  // 베스트 상품 데이터를 가져오는 API 호출
+  useEffect(() => {}, []);
+
   return (
     <div className={style['best-products']}>
       <h2 className={style['best-products__title']}>베스트 상품</h2>
