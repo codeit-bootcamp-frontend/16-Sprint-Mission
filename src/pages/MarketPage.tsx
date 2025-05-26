0ae5df5ac7147e8ff33223367d31e208c4443704
Hwigeon <bb7788@naver.com>
2025-05-26 15:08:59 +0900

    feat(MarketPage): 마켓 페이지 레이아웃 제작

--- src/pages/MarketPage.tsx
+++ src/pages/MarketPage.tsx
@@ -1,3 +1,28 @@
 import React from 'react';
-function MarketPage() {}
+import style from './MarketPage.module.scss';
+function MarketPage() {
+  return (
+    <div className={style['market-page']}>
+      <div className={style['best-products']}>
+        <h2>베스트 상품</h2>
+        <div className={style['best-products__list']}>
+          {/* 베스트 상품 리스트를 여기에 추가 */}
+        </div>
+      </div>
+      <div className={style['all-products']}>
+        <div className={style['all-products__header']}>
+          <h2>전체 상품</h2>
+          <div className={style['all-products-filter-container']}>
+            {/* 검색창 및 필터 */}
+          </div>
+        </div>
+        {/* 전체 상품 리스트 */}
+        <div className={style['all-products-list']}></div>
+
+        {/* 페이지네이션 컴포넌트 */}
+        <PageNations />
+      </div>
+    </div>
+  );
+}
 export default MarketPage;
