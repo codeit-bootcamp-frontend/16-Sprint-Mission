dd1f0e253f449093caea17e0b8aa3cb972873ff9
김성주 <aaa8757k@gmail.com>
2025-05-26 22:46:44 +0900

    refactort(mentor): pagination로직 커스텀 훅으로 빼기

---
+++ src/hooks/usePagination.js
@@ -0,0 +1,60 @@
+import { useCallback, useMemo } from "react";
+
+/*
+맨처음에 useCallback이랑 useMemo로 메모이제이션 하면 더 좋을까 하고 만들고 보니까
+사실 상 어딜 눌러도 pageStart나 page가 변해서 의미가 없는 것 같음 -> 일단 재사용이라도 가능하게 뽑아두자.
+-> 아니면 합성 컴포넌트? 이걸 알아볼까?
+ */
+export function usePagination(queryStrings, setQueryStrings, total, PAGINATION_MAX) {
+    const { page, pageSize } = queryStrings;
+
+    const maxPageLength = useMemo(() => {
+        return Math.ceil(total / pageSize);
+    }, [total, pageSize]) //이건 자주 안 바뀌니까 해도 되겠다.
+
+    const pageStart = Math.floor((page - 1) / PAGINATION_MAX) * PAGINATION_MAX + 1; /*훅에서 반환할 꺼 */
+
+    //이전, 다음 버튼 활성, 비활성 이미지용
+    const isPrevDisabled = pageStart <= 1;  /*훅에서 반환할 꺼 */
+    const isNextDisabled = pageStart + PAGINATION_MAX > maxPageLength;  /*훅에서 반환할 꺼 */
+
+    const pageList = []; /*훅에서 반환할 꺼 */
+
+    for (let i = 0; i < PAGINATION_MAX; i++) {
+        if (pageStart + i > maxPageLength) break;
+        pageList.push(pageStart + i);
+    }
+
+    const handlePrevBtn = () => { /*훅에서 반환할 꺼 */
+        const prevPageStart = pageStart - PAGINATION_MAX;
+
+        if (prevPageStart >= 1) {
+            setQueryStrings((prev) => ({
+                ...prev,
+                page: prevPageStart,
+            }));
+        }
+    }
+
+    const handleNextBtn = () => { /*훅에서 반환할 꺼 */
+        const nextPageStart = pageStart + PAGINATION_MAX;
+
+        if (maxPageLength >= nextPageStart) {
+            setQueryStrings((prev) => ({
+                ...prev,
+                page: nextPageStart,
+            }));
+        }
+    }
+
+    const handlePageRequest = useCallback((e) => { /*훅에서 반환할 꺼 */
+        const targetPage = Number(e.target.textContent);
+
+        setQueryStrings((prev) => ({
+            ...prev,
+            page: targetPage,
+        }));
+    }, [setQueryStrings])
+
+    return { pageStart, pageList, handlePageRequest, handleNextBtn, handlePrevBtn, isPrevDisabled, isNextDisabled }
+}
--- src/pages/Items/Pagination.jsx
+++ src/pages/Items/Pagination.jsx
@@ -5,76 +5,29 @@ import nextBtn_active from "@assets/images/nextBtn-active.png";
 import nextBtn_inactive from "@assets/images/nextBtn-inactive.png";
 import prevBtn_active from "@assets/images/prevBtn-active.png";
 import prevBtn_inactive from "@assets/images/prevBtn-inactive.png";
+import { usePagination } from "../../hooks/usePagination";
 
 const PAGINATION_MAX = 5;
 
 function Pagination() {
   const { queryStrings, setQueryStrings, total } = useContext(ProductData);
-  const { page, pageSize } = queryStrings;
-  const [pageList, setPageList] = useState([1, 2, 3, 4, 5]);
-  const maxPageLength = Math.ceil(total / pageSize);
 
-  function getPaginationStart() {
-    return Math.floor((page - 1) / PAGINATION_MAX) * PAGINATION_MAX + 1;
-  }
-
-  const pageStart = getPaginationStart(page);
-
-  // 밑에 이전, 다음 버튼 활성, 비활성 이미지용
-  const isPrevDisabled = pageStart <= 1;
-  const isNextDisabled = pageStart + PAGINATION_MAX > maxPageLength;
-
-  //이전으로 가기(pageStart변경)
-  function handlePrev() {
-    const prevPageStart = pageStart - PAGINATION_MAX;
-
-    if (prevPageStart >= 1) {
-      updatePagination(prevPageStart);
-    }
-  }
-
-  //다음으로 가기(pageStart변경)
-  function handleNext() {
-    const nextPageStart = pageStart + PAGINATION_MAX;
-
-    if (maxPageLength >= nextPageStart) {
-      updatePagination(nextPageStart);
-    }
-  }
-
-  //이전, 다음 누르면 페이지네이션 바꾸기
-  function updatePagination(newPageStart) {
-    const visiblePages = [];
-
-    for (let i = 0; i < PAGINATION_MAX; i++) {
-      if (newPageStart + i > maxPageLength) break;
-      visiblePages.push(newPageStart + i);
-    }
-
-    setPageList(visiblePages);
-
-    setQueryStrings((prev) => ({
-      ...prev,
-      page: newPageStart,
-    }));
-  }
-
-  // 페이지네이션 누르면 쿼리 상태 업데이트
-  function handlePageRequest(e) {
-    const targetPage = Number(e.target.textContent);
-
-    setQueryStrings((prev) => ({
-      ...prev,
-      page: targetPage,
-    }));
-  }
+  const {
+    pageStart,
+    handlePageRequest,
+    pageList,
+    handleNextBtn,
+    handlePrevBtn,
+    isPrevDisabled,
+    isNextDisabled,
+  } = usePagination(queryStrings, setQueryStrings, total, PAGINATION_MAX);
 
   return (
     <ul
       aria-label="페이지 네비게이션"
       className={styles[`items__all-pagination`]}
     >
-      <li className={styles.pagination__prev} onClick={handlePrev}>
+      <li className={styles.pagination__prev} onClick={handlePrevBtn}>
         <img
           src={isPrevDisabled ? prevBtn_inactive : prevBtn_active}
           alt="페이지 이전 목록으로 가기"
@@ -83,14 +36,14 @@ function Pagination() {
       {pageList.map((i) => (
         <li
           aria-label={`${i}페이지로 이동`}
-          className={page == i ? styles[`pagination--activate`] : ""}
+          className={queryStrings.page == i ? styles[`pagination--activate`] : ""}
           onClick={handlePageRequest}
           key={`page${i}`}
         >
           {i}
         </li>
       ))}
-      <li className={styles.pagination__next} onClick={handleNext}>
+      <li className={styles.pagination__next} onClick={handleNextBtn}>
         <img
           src={isNextDisabled ? nextBtn_inactive : nextBtn_active}
           alt="페이지 다음 목록으로 가기"
