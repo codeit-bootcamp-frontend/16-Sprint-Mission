bde9833d415cda5f25251d48ba76f22bda3cbb52
asksa1256@gmail.com <asksa1256@gmail.com>
2025-05-28 10:48:30 +0900

    feat: 중고마켓 - 새로고침 시 검색값 초기화

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -53,6 +53,14 @@ const ProductList = ({ title, pageSize = DEFAULT_ITEM_PAGE_SIZE }) => {
     });
   }, [pageSize, order, keyword, handleLoad]);
 
+  // 새로고침 시 검색값 초기화
+  useEffect(() => {
+    if (searchParams.has("keyword")) {
+      searchParams.delete("keyword");
+      setSearchParams(searchParams);
+    }
+  }, []);
+
   return (
     <div css={ProductListStyle}>
       <div className="item-list-header">
