295c640ad470b9016b71b1f3dc7fccfbd6ed33cc
Squarecat-meow <Yozumina@serafuku.moe>
2025-05-28 15:02:01 +0900

    🍱 update: function-ify Input Components

--- src/components/input.jsx
+++ src/components/input.jsx
@@ -1,19 +1,32 @@
-import './input.css';
+import "./input.css";
 
-function Input({ slot, slotDirection, slotAlt, onChange, onBlur, type, name, placeholder }) {
+function Input({
+  renderSlot,
+  slotDirection = "right",
+  onChange,
+  onBlur,
+  type,
+  name,
+  placeholder,
+}) {
   return (
-    <div className='input-container'>
-      {slot && slotDirection === 'left' && (
-        <img src={slot} alt={slotAlt} className='slot-left-image' />
+    <div className="input-container">
+      {slotDirection === "left" && renderSlot && (
+        <div className="slot-left-image">{renderSlot}</div>
       )}
-      <input className={slotDirection === 'left' ? 'input-slanted' : 'input'} onChange={onChange} type={type} name={name} onBlur={onBlur} placeholder={placeholder} />
-      {slot && slotDirection === 'right' && (
-        <button className='input-button'>
-          <img src={slot} alt={slotAlt} />
-        </button>
+      <input
+        className={slotDirection === "left" ? "input-slanted" : "input"}
+        onChange={onChange}
+        type={type}
+        name={name}
+        onBlur={onBlur}
+        placeholder={placeholder}
+      />
+      {slotDirection === "right" && renderSlot && (
+        <button className="input-button">{renderSlot}</button>
       )}
     </div>
-  )
+  );
 }
 
 export default Input;
