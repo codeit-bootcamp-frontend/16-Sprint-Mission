70809abfa5071676046097239af7906003249925
leesangdal <asksa1256@gmail.com>
2025-05-28 13:32:38 +0900

    feat: 페이지 전환 시 스크롤 위치 맨위로 이동 처리

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import { Outlet } from "react-router-dom";
 import Header from "./components/Header";
+import useScrollToTop from "./hooks/useScrollToTop";
 
 function App() {
+  useScrollToTop();
+
   return (
     <>
       <Header />
