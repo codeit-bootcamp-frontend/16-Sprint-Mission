201cddce7be01a5878028ef493c9b0351c104732
leesangdal <asksa1256@gmail.com>
2025-06-17 18:46:07 +0900

    refactor: ImageFileUploader 이미지 onLoad, onError 상태 처리 추가

--- src/components/ImageFileUploader/ImageFileUploader.jsx
+++ src/components/ImageFileUploader/ImageFileUploader.jsx
@@ -1,5 +1,5 @@
 /** @jsxImportSource @emotion/react */
-import { css } from "@emotion/react";
+import styles from "./ImageFileUploaderStyle";
 import { useEffect, useRef, useState } from "react";
 import plusIcon from "../../assets/images/ic_plus.svg";
 import closeIcon from "../../assets/images/ic_close_white.svg";
@@ -7,12 +7,14 @@ import IconButton from "../ui/Button/IconButton";
 
 const ImageFileUploader = () => {
   const [previewUrl, setPreviewUrl] = useState(null);
+  const [isImgLoading, setIsImgLoading] = useState(false);
   const imageFileRef = useRef(null);
 
   const handleFileChange = (e) => {
     const file = e.target.files[0];
     if (!file) return;
 
+    setIsImgLoading(true);
     const newPreview = URL.createObjectURL(file);
     setPreviewUrl(newPreview);
   };
@@ -37,34 +39,39 @@ const ImageFileUploader = () => {
   }, [previewUrl]);
 
   return (
-    <div css={ImageFileUploaderWrapStyle}>
-      <div css={ImageFileUploaderStyle}>
+    <div css={styles.ImageFileUploaderWrapStyle}>
+      <div css={styles.ImageFileUploaderStyle}>
         <input
           type="file"
           name="imageFileInput"
           id="productImageInput"
           accept="image/png, image/gif, image/jpeg"
           ref={imageFileRef}
-          css={FileInputStyle}
+          css={styles.FileInputStyle}
           onChange={handleFileChange}
         />
         <button
           type="button"
-          css={AddFileButtonStyle}
+          css={styles.AddFileButtonStyle}
           onClick={handleAddFileClick}
         >
           <img width="48" height="48" src={plusIcon} alt="이미지 등록 버튼" />
-          <span>이미지 등록</span>
+          <span>{isImgLoading ? "등록중..." : "이미지 등록"}</span>
         </button>
         {previewUrl && (
-          <div css={ImagePreviewStyle}>
+          <div css={styles.ImagePreviewStyle}>
             <img
               src={previewUrl}
               alt="이미지 미리보기"
               className="img-preview"
+              onLoad={() => setIsImgLoading(false)}
+              onError={(e) => {
+                e.target.onerror = null; // 이미지 로드 에러 시 무한 깜빡임 현상 방지
+                setIsImgLoading(false);
+              }}
             />
             <IconButton
-              cssOverride={FileDeleteBtnStyle}
+              cssOverride={styles.FileDeleteBtnStyle}
               imgSrc={closeIcon}
               imgAlt="첨부 이미지 제거"
               radius="round"
@@ -79,77 +86,3 @@ const ImageFileUploader = () => {
 };
 
 export default ImageFileUploader;
-
-const ImageFileUploaderWrapStyle = css`
-  .hint {
-    margin-top: 1rem;
-    color: var(--error-color);
-    font-size: 1rem;
-  }
-`;
-
-const ImageFileUploaderStyle = css`
-  display: flex;
-  gap: 10px;
-
-  @media (min-width: 768px) {
-    gap: 24px;
-  }
-`;
-
-const FileInputStyle = css`
-  display: none;
-`;
-
-const AddFileButtonStyle = css`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  gap: 12px;
-  width: calc(48% - 12px);
-  max-width: 168px;
-  aspect-ratio: 1/1;
-  flex-grow: 1;
-  background-color: var(--gray200);
-  color: var(--gray400);
-  border-radius: var(--border-radius-sm);
-  font-size: 1rem;
-
-  @media (min-width: 600px) {
-    max-width: var(--form-input-max-height);
-  }
-
-  @media (min-width: 768px) {
-    width: auto;
-    height: var(--form-input-max-height);
-    flex-grow: 0;
-  }
-`;
-
-const ImagePreviewStyle = css`
-  position: relative;
-  width: calc(48% - 12px);
-  aspect-ratio: 1/1;
-  flex-grow: 1;
-  border-radius: var(--border-radius-sm);
-  overflow: hidden;
-
-  .img-preview {
-    width: 100%;
-    height: 100%;
-    object-fit: cover;
-  }
-
-  @media (min-width: 768px) {
-    width: auto;
-    height: var(--form-input-max-height);
-    flex-grow: 0;
-  }
-`;
-
-const FileDeleteBtnStyle = css`
-  position: absolute;
-  right: 14px;
-  top: 14px;
-`;
