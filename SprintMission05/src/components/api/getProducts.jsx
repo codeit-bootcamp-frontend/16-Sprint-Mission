b8abdecc7be20c27ca815a7c826413654d0e3e21
배민지 <vvqoalswlvv@naver.com>
2025-06-18 16:44:44 +0900

    주석정리

--- SprintMission05/src/components/api/getProducts.jsx
+++ SprintMission05/src/components/api/getProducts.jsx
@@ -1,26 +1,13 @@
-//컴포넌트안에서 사용하도록
-// getProducts
-export const getProducts = async ({
-  order = "recent",
-  page = 1,
-  pageSize = 10,
-}) => {
-  const query = `page=${page}&pageSize=${pageSize}&orderBy=${order}`;
+export const getProducts = async ({ order = 'recent', page = 1, pageSize = 10 }) => {
+    const query = `page=${page}&pageSize=${pageSize}&orderBy=${order}`;
 
-  console.log(
-    "API 요청 URL 쿼리:",
-    `https://panda-market-api.vercel.app/products?${query}`
-  );
+    console.log('API 요청 URL 쿼리:', `https://panda-market-api.vercel.app/products?${query}`);
 
-  const response = await fetch(
-    `https://panda-market-api.vercel.app/products?${query}`
-  );
-  if (!response.ok) {
-    throw new Error("상품을 불러오는데 실패했습니다");
-  }
-  const body = await response.json();
-  console.log("🔥 API 응답:", body);
-  return body;
+    const response = await fetch(`https://panda-market-api.vercel.app/products?${query}`);
+    if (!response.ok) {
+        throw new Error('상품을 불러오는데 실패했습니다');
+    }
+    const body = await response.json();
+    console.log('🔥 API 응답:', body);
+    return body;
 };
-
-// useProducts 커스텀 훅 : 적용 예시가 잘못되었다-> 반복되는 로직이 즁요 : 좀 더 고민
