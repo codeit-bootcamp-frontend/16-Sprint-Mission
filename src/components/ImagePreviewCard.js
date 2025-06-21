ecf3ba222552658db51d938e7dfe78d6f7d7a457
배수민 <belly15@naver.com>
2025-06-21 21:18:22 +0900

    feat: 이미지 컴포넌트 작업

---
+++ src/components/ImagePreviewCard.js
@@ -0,0 +1,29 @@
+import "./css/ImagePreviewCard.css";
+import deleteIcon from "../img/delete.svg";
+
+const ImagePreviewCard = ({ onClickDelete = () => {}, imageUrl }) => {
+  const imageStyle = {
+    backgroundImage: `url(${imageUrl})`,
+    backgroundPosition: "center center",
+    backgroundSize: "cover",
+  };
+  const handleOnClickDelete = () => {
+    onClickDelete();
+  };
+
+  return (
+    <div
+      className="preview__card"
+      onClick={handleOnClickDelete}
+      style={imageStyle}
+    >
+      <img
+        src={deleteIcon}
+        alt="이미지 삭제 아이콘"
+        className="preview__card__icon"
+      />
+    </div>
+  );
+};
+
+export default ImagePreviewCard;
---
+++ src/components/ImageUploader.js
@@ -0,0 +1,80 @@
+import "./css/ImageUploader.css";
+import { useRef, useState } from "react";
+import ImageAddCard from "./ImageAddCard";
+import ImagePreviewCard from "./ImagePreviewCard";
+
+const ImageUploader = ({
+  onAdd = () => {},
+  onDelete = () => {},
+  onChange = () => {},
+  isValid = null,
+  message,
+}) => {
+  const statusMessageClass = {
+    null: "",
+    false: "image__uploader__message--error",
+  };
+
+  const showMessage = message && isValid === false;
+  const [imageUrl, setImageUrl] = useState("");
+  const fileInputRef = useRef(null);
+  const handleOnClickAdd = () => {
+    if (!imageUrl) {
+      fileInputRef.current.click();
+    } else {
+      /* 부모 컴포넌트에서 유효성 체크 */
+      onAdd();
+    }
+  };
+
+  const onClickDelete = () => {
+    fileInputRef.current.value = null;
+    setImageUrl("");
+    onDelete();
+  };
+
+  const onChangeFile = (e) => {
+    // 이미지 데이터 세팅 및 미리보기
+    if (e.target.files) {
+      const file = e.target.files[0];
+      onChange(file);
+      setImageUrl(URL.createObjectURL(file));
+    }
+  };
+
+  return (
+    <>
+      <div className="image__uploader">
+        <div className="image__uploader__card">
+          <ImageAddCard onClickAdd={handleOnClickAdd}>
+            <input
+              ref={fileInputRef}
+              hidden
+              type="file"
+              accept="image/*"
+              onChange={onChangeFile}
+            />
+          </ImageAddCard>
+          {imageUrl && (
+            <ImagePreviewCard
+              onClickDelete={onClickDelete}
+              imageUrl={imageUrl}
+            />
+          )}
+        </div>
+
+        {showMessage && (
+          <div
+            className={`${"image__uploader__message"} ${
+              statusMessageClass[isValid]
+            }`}
+          >
+            {message}
+          </div>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default ImageUploader;
