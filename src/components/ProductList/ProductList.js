e7a2369fac9bc1da4df79d877b1e103106fe86de
tasi <taegyung.e@gmail.com>
2025-06-24 01:27:42 +0900

    refactor(mentor): BestProductArea에서 반응형에 따라 개수 처리 되는 부분 ProductList에서 처리 되도록 수정

--- src/components/ProductList/ProductList.js
+++ src/components/ProductList/ProductList.js
@@ -1,12 +1,14 @@
 import ProductItem from "../ProductItem/ProductItem";
 import styles from "./ProductList.module.scss";
 
-const ProductList = ({ list, type = "all" }) => {
+const ProductList = ({ list, type = "all", pageSize = null }) => {
+  const productList = pageSize ? list.slice(0, pageSize) : list;
+
   return (
     <ul
       className={`${styles["productList"]} ${styles[`productList__${type}`]}`}
     >
-      {list.map((product) => (
+      {productList.map((product) => (
         <li key={product.id}>
           <ProductItem product={product} />
         </li>
