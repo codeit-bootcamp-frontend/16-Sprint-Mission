0e63f963b54dbe00d974579495d59efc1bebb896
tasi <taegyung.e@gmail.com>
2025-06-24 02:27:31 +0900

    refactor(mentor): Pagination 컴포넌트내에 불필요한 최적화 삭제

--- src/components/Pagination/Pagination.js
+++ src/components/Pagination/Pagination.js
@@ -1,34 +1,33 @@
-import { useCallback, useMemo } from "react";
+import { useEffect, useState } from "react";
 import arrow from "../../assets/images/icons/ic_pagination_arrow.svg";
 import styles from "./Pagination.module.scss";
 
 const LIMIT = 5;
 const Pagination = ({ pageSize, totalCount, currentPage, setCurrentPage }) => {
-  // 페이지네이션 5개(LIMIT)로 끊어서 2차원 배열로 생성
-  const calcPager = useMemo(() => {
+  const [currentGroup, setCurrentGroup] = useState([]);
+  const [totalPager, setTotalPager] = useState(0);
+
+  useEffect(() => {
     if (!totalCount) return;
 
-    const totalPager = Math.ceil(totalCount / pageSize);
+    const getTotalPager = Math.ceil(totalCount / pageSize); // 페이지네이션 총 개수
+    setTotalPager(getTotalPager);
 
-    const pagerArr = [];
-    let pagerCounter = 0;
-    for (let i = 0; i < totalPager; i++) {
-      if (!pagerArr[pagerCounter]) pagerArr[pagerCounter] = [];
-      pagerArr[pagerCounter].push(i + 1);
-      if (pagerArr[pagerCounter].length === LIMIT) pagerCounter++;
-    }
+    // 페이지네이션 5개(LIMIT)로 끊어서 2차원 배열로 생성
+    const pagerArr = Array.from({ length: totalPager }).reduce(
+      (acc, cur, idx) => {
+        const groupIdx = Math.floor(idx / LIMIT);
+        if (!acc[groupIdx]) acc[groupIdx] = [];
+        acc[groupIdx].push(idx + 1);
+        return acc;
+      },
+      []
+    );
 
-    return { pagerArr, totalPager };
-  }, [pageSize, totalCount]);
-
-  // currentPage 값이 있는 배열 반환
-  const getCurrentGroup = useCallback(() => {
-    if (!calcPager) return;
-
-    return calcPager.pagerArr.filter((item) => item.includes(currentPage))[0];
-  }, [calcPager, currentPage]);
-
-  const currentGroup = getCurrentGroup();
+    // currentPage 값이 있는 배열 반환
+    const currentGroup = pagerArr.find((item) => item.includes(currentPage));
+    setCurrentGroup(currentGroup);
+  }, [currentPage, totalCount, pageSize, totalPager]);
 
   // prev 버튼 클릭
   const handleClickPrev = () => {
@@ -38,7 +37,7 @@ const Pagination = ({ pageSize, totalCount, currentPage, setCurrentPage }) => {
 
   // next 버튼 클릭
   const handleClickNext = () => {
-    const totalPageNum = calcPager.totalPager;
+    const totalPageNum = totalPager;
     const changeCurrentPage =
       currentPage + 1 >= totalPageNum ? totalPageNum : currentPage + 1;
     setCurrentPage(changeCurrentPage);
@@ -76,7 +75,7 @@ const Pagination = ({ pageSize, totalCount, currentPage, setCurrentPage }) => {
             type="button"
             className={`${styles["pagination__button"]} ${styles["pagination__button-next"]}`}
             onClick={handleClickNext}
-            disabled={currentPage === calcPager.totalPager}
+            disabled={currentPage === totalPager}
           >
             <img src={arrow} alt="다음으로" />
           </button>
