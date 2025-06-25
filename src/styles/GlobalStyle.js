aad6f0dcda61d4b2fff7ae5bb70839f31b8dbfbe
sinyroom <wldmsdl7913@naver.com>
2025-06-25 19:07:04 +0900

    feat: number 타입 input 스핀 제거

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -76,7 +76,11 @@ const GlobalStyle = createGlobalStyle`
     }
   }
 
-  
+  input[type='number']::-webkit-outer-spin-button,
+  input[type='number']::-webkit-inner-spin-button {
+    -webkit-appearance: none;
+    margin: 0;
+  }
 `;
 
 export default GlobalStyle;
