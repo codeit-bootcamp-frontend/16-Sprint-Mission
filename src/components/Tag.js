c066f8ce8e3a28b95e62f54b9d581e93d7874d29
배수민 <belly15@naver.com>
2025-06-27 17:25:18 +0900

    fix: 불필요한 내부 함수 삭제

--- src/components/Tag.js
+++ src/components/Tag.js
@@ -2,14 +2,11 @@ import "./css/Tag.css";
 import deleteIcon from "../img/delete.svg";
 
 const Tag = ({ children, className, onClickDelete = () => {} }) => {
-  const deleteTag = () => {
-    onClickDelete();
-  };
   return (
     <>
       <div className={`tag ${className}`}>
         #{children}
-        <img src={deleteIcon} alt="태그 삭제 아이콘" onClick={deleteTag} />
+        <img src={deleteIcon} alt="태그 삭제 아이콘" onClick={onClickDelete} />
       </div>
     </>
   );
