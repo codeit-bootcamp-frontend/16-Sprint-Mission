ff44825d27c868658777228d30a5ac819c66b64f
tasi <taegyung.e@gmail.com>
2025-07-01 15:27:18 +0900

    chore(authUtils): 이스케이프 제거

--- src/utils/authUtils.js
+++ src/utils/authUtils.js
@@ -16,7 +16,7 @@ export function checkValidEmail(value) {
 
   // 유효성 검사
   const PATTERN =
-    /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/;
+    /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/;
   if (!PATTERN.test(value))
     return { isValid: false, msg: "잘못된 이메일 형식입니다." };
 
