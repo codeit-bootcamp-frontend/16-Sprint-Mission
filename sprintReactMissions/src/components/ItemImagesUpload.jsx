9f6c163610a06d4de27d7a58905c2554641f6a3e
kimin0401 <inptp1996@gmail.com>
2025-07-03 17:03:07 +0900

    Fix: 이미지 등록 컴포넌트에 최적화 코드 추가

--- sprintReactMissions/src/components/ItemImagesUpload.jsx
+++ sprintReactMissions/src/components/ItemImagesUpload.jsx
@@ -1,19 +1,25 @@
 //  백엔드 api 주소 https://panda-market-api.vercel.app/docs/#/ 나중에 이용
 // 상품등록하기 기능 컴포넌트
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function ItemImagesUpload() {
   const [previewUrl, setPreviewUrl] = useState(null);
   const handleImagesUpload = (e) => {
     const file = e.target.files?.[0];
     if (!file) return;
-
+    // 미리보기 URL 생성
     const url = URL.createObjectURL(file);
     setPreviewUrl(url);
-
+    //같은 파일 선택해도 change 이벤트가 발생하도록 input값 비워두기
     e.target.value = "";
   };
+  // 컴포넌트가 언마운트될 때 미리보기 URL 해제, 메모리 누수 방지용
+  useEffect(() => {
+    return () => {
+      if (previewUrl) URL.revokeObjectURL(previewUrl);
+    };
+  }, [previewUrl]);
 
   return (
     <>
