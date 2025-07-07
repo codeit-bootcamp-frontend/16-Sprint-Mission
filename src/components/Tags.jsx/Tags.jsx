3b1a665556a46b3833894c5fe79428618becbd56
3rdflr <3rdflrhtl@gmail.com>
2025-07-07 16:05:08 +0900

    DELETE all

--- src/components/Tags.jsx/Tags.jsx
+++
@@ -1,9 +0,0 @@
-import styles from './Tags.module.css'
-
-export default function Tags({tags}) {
-  return (<div className={styles.tags}>
-  {tags.map((tag,i) =>
-  <div key={i}>
-    <p className={styles.tag}>#{tag}</p>
-  </div>)}
-  </div >)}
