684affa5b29afe7068e0d0e0152426e028cc84ca
asksa1256@gmail.com <asksa1256@gmail.com>
2025-07-08 14:05:58 +0900

    refactor: 폼 필드 타입 생성

--- src/types/form.ts
+++ src/types/form.ts
@@ -1,3 +1,16 @@
-export type ReqData = {
+import { ChangeEvent, RefObject } from "react";
+
+export interface FormField {
+  label: string;
+  inputId?: string;
+  type?: string;
+  name: string;
+  placeholder: string;
+  required?: boolean;
+  form: RefObject<HTMLFormElement | null>;
+  onBlur?: (e: ChangeEvent<HTMLInputElement>) => void;
+}
+
+export interface ReqData {
   [key: string]: FormDataEntryValue;
-};
+}
