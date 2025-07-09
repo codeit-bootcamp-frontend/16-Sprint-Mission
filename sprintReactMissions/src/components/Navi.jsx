b03255a7c02a6b088b4fc08ec330429549055e04
kimin0401 <inptp1996@gmail.com>
2025-07-09 18:18:32 +0900

    Style: 네비게이션바 스타일링 개선

--- sprintReactMissions/src/components/Navi.jsx
+++ sprintReactMissions/src/components/Navi.jsx
@@ -1,8 +1,12 @@
 // 상단 네비게이션바
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import "../styles/Navi.css";
 
 const Navi = () => {
+  const location = useLocation();
+
+  const isAddItemPage = location.pathname === "/additem";
+
   return (
     <nav className="navi">
       <div className="logo-section">
@@ -13,7 +17,10 @@ const Navi = () => {
           <Link to="/free-board" className="nav-link">
             자유게시판
           </Link>
-          <Link to="/items" className="nav-link">
+          <Link
+            to="/additem"
+            className={`nav-link ${isAddItemPage ? "highlight" : ""}`}
+          >
             중고마켓
           </Link>
         </div>
