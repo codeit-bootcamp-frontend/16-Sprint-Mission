3c752a9467f27d15159c02ea530b7b9c3325d4e6
asksa1256@gmail.com <asksa1256@gmail.com>
2025-07-13 22:45:14 +0900

    chore: 컬러, 폰트 크기 테일윈드 설정 + 필요없는 파일 삭제

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,41 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: "#7C3AED",
+          100: "#EDE9FE",
+        },
+        gray: {
+          100: "#F1F5F9",
+          200: "#E2E8F0",
+          300: "#E2E8F0",
+          400: "#94A3B8",
+          500: "#64748B",
+          800: "#1E293B",
+          900: "#0F172A",
+        },
+        rose: {
+          DEFAULT: "#F43F5E",
+        },
+        lime: {
+          DEFAULT: "#BEF264",
+        },
+        amber: {
+          DEFAULT: "#92400E",
+        },
+      },
+      fontSize: {
+        base: "16px",
+        lg: "18px",
+        xl: "20px",
+      },
+    },
+  },
+  screen: {
+    md: "640px",
+    lg: "1280px",
   },
   plugins: [],
 };
