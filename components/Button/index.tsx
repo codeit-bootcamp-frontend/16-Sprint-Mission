a5e60e6cba961a299a7c1983601cc8af1062f612
leesangdal <asksa1256@gmail.com>
2025-07-14 13:09:34 +0900

    style: Button border 색상 커스텀 컬러로 변경

--- components/Button/index.tsx
+++ components/Button/index.tsx
@@ -8,22 +8,24 @@ interface ButtonProps {
 }
 
 const baseButtonStyle =
-  "w-[164px] h-[52px] flex items-center justify-center text-base font-bold rounded-3xl border-2 border-black shadow-button";
+  "w-[164px] h-[52px] flex items-center justify-center text-base font-bold rounded-3xl border-2 border-gray-900 shadow-button";
 
 const Button = ({ type, variant, children, disabled }: ButtonProps) => {
   if (!disabled) {
     if (variant === "primary") return <PrimaryButton>{children}</PrimaryButton>;
     if (variant === "danger") return <DangerButton>{children}</DangerButton>;
-  } else
-    return (
-      <button
-        type={type}
-        disabled={disabled}
-        className={`${baseButtonStyle} text-gray-900`}
-      >
-        {children}
-      </button>
-    );
+  }
+
+  // disabled button
+  return (
+    <button
+      type={type}
+      disabled={disabled}
+      className={`${baseButtonStyle} bg-gray-200 text-gray-900`}
+    >
+      {children}
+    </button>
+  );
 };
 
 export default Button;
