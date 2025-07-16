5b605cb0b3d49945f06e12d589c23e221d37fdf2
tasi <taegyung.e@gmail.com>
2025-07-17 02:57:53 +0900

    feature(ProductDetailPage): 상품 상세페이지 추가

--- src/pages/ProductDetailPage/ProductDetailPage.tsx
+++ src/pages/ProductDetailPage/ProductDetailPage.tsx
@@ -1,5 +1,32 @@
+/** @jsxImportSource @emotion/react */
+import styled from "@emotion/styled/macro";
+import ProductInfo from "@pages/ProductDetailPage/components/ProductInfo";
+import ProductInquiry from "@pages/ProductDetailPage/components/ProductInquiry";
+import { mq } from "@styles/mixins";
+
 const ProductDetailPage = () => {
-  return <div>ProductDetailPage</div>;
+  return (
+    <div id="container">
+      <Inner>
+        <ProductInfo />
+        <ProductInquiry />
+      </Inner>
+    </div>
+  );
 };
+
+const Inner = styled.div`
+  max-width: 1200px;
+  width: calc(100% - 48px);
+  margin: 0 auto;
+  padding-top: 24px;
+  padding-bottom: 100px;
+
+  ${mq["mobile"]} {
+    width: calc(100% - 30px);
+    padding-top: 16px;
+    padding-bottom: 50px;
+  }
+`;
 
 export default ProductDetailPage;
