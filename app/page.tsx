3ac1598ff87a1762bdbcb762d8ac688da8919e60
leesangdal <asksa1256@gmail.com>
2025-08-05 17:39:23 +0900

    feat: 투두리스트 Suspense 추가, fallback skeleton ui 생성

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,16 @@
 import TodoClient from "@/components/Todos/TodoClient";
 import getTodos from "@/components/Todos/getTodos";
+import { Suspense } from "react";
+import TodoLoading from "@/components/Todos/TodoLoading";
 
 const Home = async () => {
   const initialItems = await getTodos();
 
-  return <TodoClient initialItems={initialItems} />;
+  return (
+    <Suspense fallback={<TodoLoading />}>
+      <TodoClient initialItems={initialItems} />
+    </Suspense>
+  );
 };
 
 export default Home;
