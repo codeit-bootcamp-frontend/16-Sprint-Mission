8c92cdf8fc674831c3fb491b21d70fc99299e021
leesangdal <asksa1256@gmail.com>
2025-08-05 14:28:43 +0900

    refactor: 테일윈드 버튼 커스텀 클래스 생성 및 clsx 적용

--- components/Button/index.tsx
+++ components/Button/index.tsx
@@ -1,4 +1,5 @@
 import { ReactNode } from "react";
+import clsx from "clsx";
 
 interface ButtonProps {
   type?: "submit" | "reset" | "button" | undefined;
@@ -7,30 +8,17 @@ interface ButtonProps {
   disabled?: boolean;
 }
 
-const baseButtonStyle =
-  "w-[164px] h-[52px] flex items-center justify-center text-base font-bold rounded-3xl border-2 border-gray-900 shadow-button duration-150";
-
 const Button = ({ type, variant, children, disabled }: ButtonProps) => {
-  const getVariantStyle = () => {
-    if (disabled) {
-      return "bg-gray-200 text-gray-900";
-    }
-
-    switch (variant) {
-      case "primary":
-        return "bg-primary text-white hover:bg-primary-dark";
-      case "danger":
-        return "bg-danger text-white hover:bg-danger-dark";
-      default:
-        return "bg-gray-200 text-gray-900";
-    }
-  };
-
   return (
     <button
       type={type}
       disabled={disabled}
-      className={`${baseButtonStyle} ${getVariantStyle()}`}
+      className={clsx("btn-base", {
+        "btn-primary": variant === "primary" && !disabled,
+        "btn-success": variant === "success" && !disabled,
+        "btn-danger": variant === "danger" && !disabled,
+        "btn-disabled": disabled,
+      })}
     >
       {children}
     </button>
