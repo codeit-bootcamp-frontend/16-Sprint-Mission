11eae3ff3dd027bc199029b532ecd1b19afdf60a
leesangdal <asksa1256@gmail.com>
2025-08-06 22:51:18 +0900

    feat: 이미지 업로드 커스텀 훅 생성

---
+++ hooks/useImageUpload.ts
@@ -0,0 +1,22 @@
+import { useMutation } from "@tanstack/react-query";
+import { TENANT_ID } from "@/constants/constants";
+import axios from "axios";
+
+const useImageUpload = () => {
+  return useMutation({
+    mutationFn: async (file: File) => {
+      const formData = new FormData();
+      formData.append("image", file);
+
+      const res = await axios.post(`/${TENANT_ID}/images/upload`, formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      });
+
+      return res.data;
+    },
+  });
+};
+
+export default useImageUpload;
