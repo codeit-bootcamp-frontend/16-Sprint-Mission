8a06d83610c74e8b53fa5e0f21278c7060762512
leesangdal <asksa1256@gmail.com>
2025-08-09 22:52:59 +0900

    feat: 수정 폼 - 수정 상태에 따라 '수정하기' 버튼 활성화/비활성화 처리

--- components/Todos/TodoUpdateForm.tsx
+++ components/Todos/TodoUpdateForm.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import BulletButton from "@/components/Button/BulletButton";
-import { FormEvent, useState } from "react";
+import { FormEvent, useEffect, useState } from "react";
 import { UpdateItem } from "@/types/todo";
 import ImageUploader from "@/components/ImageUploader";
 import MemoContainer from "@/components/MemoContainer";
@@ -9,12 +9,12 @@ import Button from "@/components/Button";
 import ChkIcon from "@/assets/images/ico-check.svg";
 import DeleteIcon from "@/assets/images/ico-x.svg";
 
-const TodoUpdateForm = ({ initialData }: { initialData: UpdateItem }) => {
-  const [data, setData] = useState(initialData);
+const TodoUpdateForm = ({ initialData: data }: { initialData: UpdateItem }) => {
   const [name, setName] = useState(data.name);
   const [image, setImage] = useState<string | undefined>(data.imageUrl);
   const [memo, setMemo] = useState(data.memo);
   const [isCompleted, setIsCompleted] = useState(data.isCompleted);
+  const [isUpdated, setIsUpdated] = useState(false);
   const variant = data.isCompleted ? "done" : "todo";
 
   const handleSubmit = (e: FormEvent) => {
@@ -32,12 +32,30 @@ const TodoUpdateForm = ({ initialData }: { initialData: UpdateItem }) => {
     // patch item...
   };
 
+  useEffect(() => {
+    const checkUpdate =
+      data.name !== name ||
+      data.imageUrl !== image ||
+      data.isCompleted !== isCompleted ||
+      data.memo !== memo;
+
+    setIsUpdated(checkUpdate);
+  }, [data, name, image, isCompleted, memo]);
+
   return (
     <form onSubmit={handleSubmit} className="flex flex-col">
       <div className="item-base flex justify-center mb-6 rounded-3xl cursor-default">
-        <BulletButton variant={variant} />
+        <BulletButton variant={variant} type="button" />
         <h2 className="py-2 text-base font-extrabold underline underline-offset-4">
-          {name}
+          <input
+            name="name"
+            value={name}
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
+            className="block bg-transparent field-sizing-content"
+            autoComplete="off"
+          />
         </h2>
       </div>
 
@@ -47,7 +65,7 @@ const TodoUpdateForm = ({ initialData }: { initialData: UpdateItem }) => {
       </div>
 
       <div className="flex gap-4 mt-6 self-end">
-        <Button variant="success" disabled={false} type="submit">
+        <Button variant="success" disabled={!isUpdated} type="submit">
           <ChkIcon className="w-4 h-4 mr-1" /> 수정 완료
         </Button>
         <Button variant="danger">
