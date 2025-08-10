19f046f69aab0e69721dd7e078c9074e194f1e4a
summerdev96 <developingsummer96@gmail.com>
2025-08-10 22:34:20 +0900

    chore: GA 코드 GTM으로 변경

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,34 +1,38 @@
 import '@/app/globals.css';
+
 import Script from 'next/script';
 
 import Gnb from '@/components/Gnb';
 import QueryProvider from '@/components/QueryProvider';
-import * as gtag from '@/lib/gtag';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang='ko'>
-      <body>
-        {/* Google tag (gtag.js)*/}
+      <head>
+        {/* <!-- Google Tag Manager (noscript) --> */}
         <Script
-          async
-          src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
-        ></Script>
-        <Script
-          id='gtag'
+          id='gtm'
           strategy='afterInteractive'
           dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', '${gtag.GA_TRACKING_ID}', {
-                page_path: window.location.pathname,
-              });
-          `,
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-MLCFLVF2');`,
           }}
-        />
+        ></Script>
+      </head>
 
+      <body>
+        {/* <!-- Google Tag Manager (noscript) --> */}
+        <noscript>
+          <iframe
+            src={'https://www.googletagmanager.com/ns.html?id=GTM-MLCFLVF2'}
+            height='0'
+            width='0'
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
         <QueryProvider>
           <Gnb />
           {children}
