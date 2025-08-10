9a2329e3e19f6770e8de276f9c753cbe6a9c7964
summerdev96 <developingsummer96@gmail.com>
2025-08-10 21:18:37 +0900

    feat: GA 코드 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,34 @@
 import '@/app/globals.css';
+import Script from 'next/script';
+
 import Gnb from '@/components/Gnb';
 import QueryProvider from '@/components/QueryProvider';
+import * as gtag from '@/lib/gtag';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang='ko'>
       <body>
+        {/* Google tag (gtag.js)*/}
+        <Script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+        ></Script>
+        <Script
+          id='gtag'
+          strategy='afterInteractive'
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${gtag.GA_TRACKING_ID}', {
+                page_path: window.location.pathname,
+              });
+          `,
+          }}
+        />
+
         <QueryProvider>
           <Gnb />
           {children}
