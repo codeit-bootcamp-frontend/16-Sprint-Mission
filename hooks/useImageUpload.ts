f474bf501760f990ea4d85fe04a001f5465ace2a
leesangdal <asksa1256@gmail.com>
2025-08-11 16:37:27 +0900

    chore: 불필요한 주석 제거

--- hooks/useImageUpload.ts
+++ hooks/useImageUpload.ts
@@ -5,17 +5,6 @@ import axios from "@/lib/axios";
 const useImageUpload = () => {
   return useMutation({
     mutationFn: async (file: File) => {
-      // // 영어 파일명만 가능
-      // const filename = file.name.split(".").slice(0, -1).join(".");
-      // const engOnlyRegex = /^[a-zA-Z]+$/;
-      // if (!engOnlyRegex.test(filename))
-      //   throw new Error("파일 이름은 영어로만 이루어져야 합니다.");
-
-      // // 파일 크기 제한
-      // const MAX_SIZE = 5 * 1024 * 1024;
-      // if (file.size > MAX_SIZE)
-      //   throw new Error("파일 크기는 5MB 이하여야 합니다.");
-
       const formData = new FormData();
       formData.append("image", file);
 
