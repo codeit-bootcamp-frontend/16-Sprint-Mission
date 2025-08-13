cb55b2fae2eeea406292a38d1d7981c15f37af44
tasi <taegyung.e@gmail.com>
2025-08-13 18:32:27 +0900

    refactor(mentor): 투두추가폼 api 동작을 refresh() -> 서버액션 + revalidateTag로 수정

--- src/features/todo/services/todoApi.ts
+++ src/features/todo/services/todoApi.ts
@@ -10,7 +10,9 @@ const API_END_POINT = process.env.NEXT_PUBLIC_SERVER_COMMON_END_POINT;
 const BASE_URL = `${API_URL}${API_END_POINT}`;
 
 export async function getTodoList(): Promise<TodoItemType[]> {
-  const res = await fetch(`${BASE_URL}/items`);
+  const res = await fetch(`${BASE_URL}/items`, {
+    next: { tags: ["todoList"] },
+  });
 
   if (!res.ok) throw new Error(res.statusText);
 
