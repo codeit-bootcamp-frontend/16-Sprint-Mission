4b82fd029ed76a5aae3d82c5293bf6dd35a94ac4
leesangdal <asksa1256@gmail.com>
2025-08-14 23:24:02 +0900

    feat: axios interceptor - 401 권한 에러시 토큰 재발급 설정

--- constants/index.ts
+++ constants/index.ts
@@ -5,8 +5,3 @@ export const BREAKPOINTS = {
 };
 
 export const apiUrl = process.env.NEXT_PUBLIC_API_URL!;
-
-export const loginErrorMsg = {
-  email: "존재하지 않는 이메일입니다.",
-  password: "비밀번호가 일치하지 않습니다.",
-};
---
+++ utils/apiClient.ts
@@ -0,0 +1,61 @@
+import axios from "axios";
+import { apiUrl } from "@/constants";
+import { useAuthStore } from "@/stores/authStore";
+
+const { clearUser } = useAuthStore.getState();
+
+const apiClient = axios.create({
+  baseURL: apiUrl,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+// 요청 헤더에 액세스 토큰 추가
+apiClient.interceptors.request.use((config) => {
+  if (typeof window !== "undefined") {
+    const token = localStorage.getItem("accessToken");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+  }
+  return config;
+});
+
+// 응답 401 에러 (액세스 토큰 만료) 발생 시 토큰 재발급
+apiClient.interceptors.response.use(
+  (res) => res,
+  async (error) => {
+    const originalReq = error.config;
+    const status = error?.status || error?.response?.status;
+
+    if (typeof window === "undefined") return;
+
+    const refreshToken = localStorage.getItem("refreshToken");
+
+    if (refreshToken && status === 401 && !originalReq._retry) {
+      originalReq._retry = true; // 요청 무한 루프 방지
+
+      try {
+        const res = await axios.post("/auth/refresh-token", refreshToken);
+        localStorage.setItem("accessToken", res.data.accessToken);
+        originalReq.headers.Authorization = `Bearer ${res.data.accessToken}`;
+
+        return apiClient(originalReq);
+      } catch (err) {
+        // 액세스 토큰 재발급 실패: 토큰 삭제 + 로그아웃
+        await fetch("/api/auth/logout", {
+          method: "POST",
+          credentials: "include",
+        });
+        clearUser();
+
+        return Promise.reject(error);
+      }
+    }
+
+    return Promise.reject(error);
+  }
+);
+
+export default apiClient;
